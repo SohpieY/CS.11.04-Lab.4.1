ef8a392da748f230f7fbc4aadbbf3782c2a04694
tianlinsy <tianlinsy2026@student.cis.edu.hk>
2024-06-04 17:15:10 +0800

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,118 +1,91 @@
 public class Main {
+    public static void main(String[] args) {
 
+    }
     // 1. parenthesesCheck
-    public static boolean parenthesesCheck(String str) {
+    public static boolean parenthesesCheck(String str){
         int count = 0;
-        for(int i = 0; i < str.length(); i++){
-            char checkCurrent = str.charAt(i);
-            if(checkCurrent == '('){
-                count++;
-            } else if(checkCurrent == ')'){
-                count--;
-            }
-            if(count < 0){
-                return false;
-            }
-        }
-        return count == 0;
 
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') count++;
+            else if (str.charAt(i) == ')') count--;
+        }
+        return (count == 0);     //each '(' should have a pairing ')', thus the final count = 0
     }
 
     // 2. reverseInteger
-    public static String reverseInteger(int num){
-        String numString = Integer.toString(num);
-        String result = "";
-        for(int i = numString.length()- 1; i >= 0; i--){
-            result += numString.charAt(i);
-        }
-        return result;
+    public static String reverseInteger(int digits){
+        String str = String.valueOf(digits);    //turning num into string
+        String out = "";
+
+        //starting from last digit and adding it to the final output
+        for (int i = str.length()-1; i > -1; i--) out += str.charAt(i);
+
+        return out;
     }
 
     // 3. encryptThis
-    public class MessageEncryptor {
-        public static String encryptMessage(String message) {
-            StringBuilder encryptedMessage = new StringBuilder();
-            StringBuilder word = new StringBuilder();
+    public static String encryptThis(String str){
+        str += " ";
+        String str2 = " " + str + " ";
+        String out = "";
+        int y = 0;
 
-            for (int i = 0; i < message.length(); i++) {
-                char c = message.charAt(i);
-
-                if (c == ' ') {
-                    String encryptedWord = encryptWord(word.toString());
-                    encryptedMessage.append(encryptedWord).append(' ');
-
-                    word.setLength(0);
-                } else {
-                    word.append(c);
-                }
-            }
-
-
-            if (!word.isEmpty()) {
-                String encryptedWord = encryptWord(word.toString());
-                encryptedMessage.append(encryptedWord);
-            }
-
-            return encryptedMessage.toString();
-        }
-
-        private static String encryptWord(String word) {
-            int firstLetterCode = (int) word.charAt(0);
-            String switchedLetters = word.charAt(word.length() - 1) + word.substring(1, word.length() - 1) + word.charAt(0);
-            return firstLetterCode + switchedLetters;
-        }
-
-        public static void main(String[] args) {
-            String message = "Hello world";
-            String encryptedMessage = encryptMessage(message);
-            System.out.println(encryptedMessage);
-        }
-    }
-
-    // 4. decipherThis
-    // got some help
-    public class MessageDecipher {
-        public static String decipherMessage(String message) {
-            String[] words = message.split(" ");
-            StringBuilder decipheredMessage = new StringBuilder();
-
-            for (String word : words) {
-                StringBuilder decipheredWord = new StringBuilder();
-
-                if (word.length() > 0) {
-                    int firstLetterCode = 0;
-
-                    for (int i = 0; i < word.length(); i++) {
-                        char c = word.charAt(i);
-
-                        if (Character.isDigit(c)) {
-                            firstLetterCode = firstLetterCode * 10 + Character.getNumericValue(c);
-                        } else if (i == 0) {
-                            decipheredWord.append((char) firstLetterCode);
-                        } else if (i == 1) {
-                            decipheredWord.append(word.charAt(word.length() - 1));
-                        } else if (i == word.length() - 1) {
-                            decipheredWord.append(word.charAt(1));
-                        } else {
-                            decipheredWord.append(c);
-                        }
+        for (int i = 1; i < str2.length()-2; i++) {
+            if (str2.charAt(i - 1) == ' ') out += (int) str2.charAt(i);   //first char converting to int
+            else if (str2.charAt(i - 2) == ' ') {           //second char
+                y = i;
+                for (int j = i; j < str2.length(); j++) {
+                    if (str2.charAt(j) == ' ') {    //until the end of the word
+                        out += str2.charAt(j-1);      //will get the last letter of that word
+                        break;
                     }
                 }
-
-                decipheredMessage.append(decipheredWord).append(" ");
             }
-
-            return decipheredMessage.toString().trim();
+            else if (str2.charAt(i + 1) == ' ') {             //last char
+                out += str2.charAt(y);      //gets the noted second letter from the previous else-if
+            }
+            else out += str2.charAt(i);
         }
 
-        public static void main(String[] args) {
-            String message1 = "72olle 103doo 100ya";
-            String decipheredMessage1 = decipherMessage(message1);
-            System.out.println(decipheredMessage1);
+        return out;
+    }
 
-            String message2 = "82yade 115te 103o";
-            String decipheredMessage2 = decipherMessage(message2);
-            System.out.println(decipheredMessage2);
+
+    // 4. decipherThis
+    public static String decipherThis(String str) {
+        String str2 = " " + str + " ";
+        String out = "";
+        int y = 0;
+        int count = 0;
+
+        for (int i = 1; i < str2.length()-1; i++) {
+            if (str2.charAt(i-1) == ' ') {        // first char
+                for (int j = i; j < str2.length(); j++) {
+                    boolean ascii = Character.isDigit(str2.charAt(j));   // if char is a number
+                    if (!ascii) {
+                        out += (char) Integer.parseInt(str2.substring(i, j));    // convert int to char
+                        break;
+                    }
+                    count++;   //num of ascii digits
+                }
+                i += count;
+            }
+            if (str2.charAt(i-count-1) == ' ') {
+                y = i;
+                for (int k = i; k < str2.length(); k++) {
+                    if (str2.charAt(k+1) == ' ') {   // original second char
+                        out += str2.charAt(k);
+                        break;
+                    }
+                }
+                count = 0;
+            }
+            else if (str2.charAt(i+1) == ' ') {             // last char
+                out += str2.charAt(y);      // adds original last char which is at y
+            }
+            else out += str2.charAt(i);
         }
+        return out;
     }
 }
